884b50352114103c37b06b0fb05e7f3b701271e2
Alyssia Chen <84742988+alyssia-chen@users.noreply.github.com>
2022-04-12 09:43:26 -1000

    Clicking on the logo redirects to landing

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -12,7 +12,7 @@ class NavBar extends React.Component {
     const menuStyle = { marginBottom: '10px', backgroundColor: '#024731' };
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
-        <Menu.Item>
+        <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Image src='/images/LogoTransparent.png' className='tiny'/>
         </Menu.Item>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
