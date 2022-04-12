e4bbf6ffebfbc34efb9adf35caa281438da2bd8e
Kobeyarai808 <kobeyarai@gmail.com>
2022-04-11 17:34:28 -1000

    Fixed user profile

--- app/imports/ui/components/UserProfile.jsx
+++ app/imports/ui/components/UserProfile.jsx
@@ -1,22 +1,60 @@
 import React from 'react';
-import { Grid, Icon } from 'semantic-ui-react';
+import { Grid, Header, Icon, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import StuffItem from './StuffItem';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class UserProfile extends React.Component {
   render() {
     return (
       <Grid style={{ margin: '20px' }}>
-        <Grid.Column width={4}>
-          <div style={{ height: '750px', backgroundColor: 'grey' }}>
+        <Grid.Column width={4} textAlign='center'>
+          <div style={{ height: '750px', backgroundColor: 'grey', paddingTop: '30px' }}>
             <Icon name='user' size='massive'/>
-            <p>Hello, World!</p>
           </div>
         </Grid.Column>
         <Grid.Column width={12}>
-          <div style={{ height: '750px', backgroundColor: 'green' }}>
-            <p>Hello, World!</p>
+          <div style={{ height: '750px', paddingTop: '30px' }}>
+            <Header as='h1'>Johnny Appleseed</Header>
+            <p>Driver<br/>Waimalu, 96701</p>
+            <Header as="h2" textAlign="center">Availability</Header>
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Day of the Week</Table.HeaderCell>
+                  <Table.HeaderCell>UHM Arrival Time</Table.HeaderCell>
+                  <Table.HeaderCell>UHM Departure Time</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>Monday</Table.Cell>
+                  <Table.Cell>10:00AM</Table.Cell>
+                  <Table.Cell>10:00PM</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Tuesday</Table.Cell>
+                  <Table.Cell>10:00AM</Table.Cell>
+                  <Table.Cell>10:00PM</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Wednesday</Table.Cell>
+                  <Table.Cell>10:00AM</Table.Cell>
+                  <Table.Cell>10:00PM</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Thursday</Table.Cell>
+                  <Table.Cell>10:00AM</Table.Cell>
+                  <Table.Cell>10:00PM</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Friday</Table.Cell>
+                  <Table.Cell>10:00AM</Table.Cell>
+                  <Table.Cell>10:00PM</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </div>
         </Grid.Column>
       </Grid>
