5cf96bc17ad845fb853b58f2c8dafb81a481bac8
yongsungm <97987234+yongsungm@users.noreply.github.com>
2022-04-14 09:44:16 -1000

    #11 Create list reports page

---
+++ app/imports/ui/pages/ListReports.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Table, Header, Loader } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Stuffs } from '../../api/stuff/Stuff';
+
+class ListReports extends React.Component {
+
+  // If the subscription(s) have been received, render the page, otherwise show a loading icon.
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  // Render the page once subscriptions have been received.
+  renderPage() {
+    return (
+      <Container>
+        <Header as="h2" textAlign="center">Review Reports</Header>
+        <Table celled>
+          <Table.Body>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>user</Table.HeaderCell>
+                <Table.HeaderCell>reported by</Table.HeaderCell>
+                <Table.HeaderCell>issue</Table.HeaderCell>
+                <Table.HeaderCell>date</Table.HeaderCell>
+                <Table.HeaderCell>reported message</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>jappleseed@hawaii.edu</Table.HeaderCell>
+              <Table.HeaderCell>jdeer@hawaii.edu</Table.HeaderCell>
+              <Table.HeaderCell>Broke TOS</Table.HeaderCell>
+              <Table.HeaderCell>Mar 13, 2022</Table.HeaderCell>
+              <Table.HeaderCell><a>message link/modal</a></Table.HeaderCell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </Container>
+    );
+  }
+}
+
+// Require an array of Stuff documents in the props.
+ListReports.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Stuffs.adminPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const stuffs = Stuffs.collection.find({}).fetch();
+  return {
+    stuffs,
+    ready,
+  };
+})(ListReports);
