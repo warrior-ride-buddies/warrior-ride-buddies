21bcdf77bcc9dabe326af99aa7177229175efecc
William Liang <williamliang808@gmail.com>
2022-04-20 17:21:55 -1000

    Adds time functionality and sweet alert

---
+++ app/imports/ui/components/TimePicker.jsx
@@ -0,0 +1,17 @@
+// import React, { useState } from 'react';
+// import SemanticDatepicker from 'react-semantic-ui-datepickers';
+// import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
+//
+// const AppWithBasic = () => {
+//   const [currentDate, setNewDate] = useState(null);
+//   const onChange = (event, data) => setNewDate(data.value);
+//
+//   return <SemanticDatepicker onChange={onChange} />;
+// };
+//
+// const AppWithRangeAndInPortuguese = () => {
+//   const [currentRange, setNewRange] = useState([]);
+//   const onChange = (event, data) => setNewRange(data.value);
+//
+//   return <SemanticDatepicker locale="pt-BR" onChange={onChange} type="range" />;
+// };
