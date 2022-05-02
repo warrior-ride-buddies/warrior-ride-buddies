59e576cf4deb8d9a47d2d85ef61c5cf632cb90c5
yongsungm <97987234+yongsungm@users.noreply.github.com>
2022-05-01 14:14:03 -1000

    merge fix

--- app/imports/api/parse/parse.js
+++ app/imports/api/parse/parse.js
@@ -19,6 +19,37 @@ class Parse {
     const conversion = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return conversion.indexOf(string);
   }
+
+  static dayToString(num) {
+    let day = '';
+    switch (num) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+      day = 'undefined';
+      break;
+    }
+    return day;
+  }
 }
 
 export default Parse;
